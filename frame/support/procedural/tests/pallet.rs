3a7312c13269d693d06e4f916c227fb759270cb8
thiolliere <gui.thiolliere@gmail.com>
2020-07-17 16:41:57 +0200

    WIP

---
+++ frame/support/procedural/tests/pallet.rs
@@ -0,0 +1,24 @@
+#[frame_support_procedural::pallet]
+mod Pallet {
+	use frame_support::pallet_prelude::*;
+
+	#[pallet::trait_]
+	trait Trait<I: Instance = DefaultInstance> {
+		#[pallet::const_]
+		type Too: Get<u32>;
+	}
+
+	#[pallet::module]
+	pub struct Module<T, I = DefaultInstance>(core::marker::PhantomData::<(T, I)>);
+
+	#[pallet::module_interface]
+	impl<T: Trait<I>, I: Instance> ModuleInterface for Module<T, I> {
+	}
+
+	#[pallet::call]
+	impl<T: Trait<I>, I: Instance> Call for Module<T, I> {
+		#[pallet::weight = 0]
+		fn toto(origin: OriginFor<T>, #[pallet::compact] toto: u32) -> DispatchResultWithPostInfo {
+		}
+	}
+}
