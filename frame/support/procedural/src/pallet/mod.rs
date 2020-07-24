4f4f08448a2dc0eac0ef9261f3dac22c7f226ef1
thiolliere <gui.thiolliere@gmail.com>
2020-07-24 02:36:24 +0200

    WIP

--- frame/support/procedural/src/pallet/mod.rs
+++ frame/support/procedural/src/pallet/mod.rs
@@ -1,6 +1,6 @@
 // This file is part of Substrate.
 
-// Copyright (C) 2019-2020 Parity Technologies (UK) Ltd.
+// Copyright (C) 2020 Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,12 +15,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! Implementation for pallet attribute macro.
+//!
+//! General workflow:
+//! 1 - parse all pallet attributes:
+//!   This step remove all attributes `#[pallet::*]` from the ItemMod and build the `Def` struct
+//!   which holds the ItemMod without `#[pallet::*]` and information given by those attributes
+//! 2 - expand from the parsed information
+//!   This step will modify the ItemMod by adding some derive attributes or phantom data variants
+//!   to user defineds types. And also crate new types and implement block.
+
 mod parse;
 mod expand;
 
 pub use parse::Def;
 
-pub fn pallet(_attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn pallet(
+	_attr: proc_macro::TokenStream,
+	item: proc_macro::TokenStream
+) -> proc_macro::TokenStream {
 	let item = syn::parse_macro_input!(item as syn::ItemMod);
 	match parse::Def::try_from(item) {
 		Ok(def) => expand::expand(def).into(),
